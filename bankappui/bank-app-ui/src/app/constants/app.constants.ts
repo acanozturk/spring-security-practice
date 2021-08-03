bb09baf6838ca159fcd1b3b3c9674390666b743c
Ali Can Öztürk <acanozturk96@gmail.com>
2021-08-03 17:09:35 +0300

    Added frontend

---
+++ bankappui/bank-app-ui/src/app/constants/app.constants.ts
@@ -0,0 +1,10 @@
+
+export const  AppConstants = {
+    CONTACT_API_URL : "/contact",
+    LOGIN_API_URL : "/login",
+    ACCOUNT_API_URL : "/account",
+    BALANCE_API_URL : "/transactions",
+    LOANS_API_URL : "/loans",
+    CARDS_API_URL : "/cards",
+    NOTICES_API_URL : "/notices"
+}
